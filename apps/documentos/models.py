9c48cd57274743ba57bd0bfb634358343a8ffbcc
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-05-09 19:45:48 -0300

    Criando os relacionamentos do banco de dados

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+# importando App Funcionarios 
+from apps.funcionarios.models import Funcionario
 
 # Create your models here.
 class Documento(models.Model):
     description = models.CharField(max_length=100)
+    # adicionando a classe pertence importada pela app Funcionarios
+    belong = models.ForeignKey(
+        Funcionario,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.description
--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User #importando modulo AUTH User
+# importando app departamentos
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
 
-
-# Create your models here.
+# Create your models here.a
 class Funcionario(models.Model):
     name = models.CharField(max_length=100)
+    username = models.OneToOneField(User, on_delete=models.PROTECT, unique=True)
+    # adicionando a classe departamentos que foi importada apartir de apps.
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.name
