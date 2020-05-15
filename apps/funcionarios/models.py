c9ead5431aa4a2bbea5275848b58d003aea7c695
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-05-15 20:51:16 -0300

    Finish adding Crud Funcionarios and Update

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User #importando modulo AUTH User
 # importando app departamentos
 from apps.departamentos.models import Departamento
 from apps.empresas.models import Empresa
+from django.urls import reverse
 
 # Create your models here.a
 class Funcionario(models.Model):
@@ -11,6 +12,9 @@ class Funcionario(models.Model):
     # adicionando a classe departamentos que foi importada apartir de apps.
     departamentos = models.ManyToManyField(Departamento)
     empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('list_funcionarios')
 
     def __str__(self):
         return self.name
