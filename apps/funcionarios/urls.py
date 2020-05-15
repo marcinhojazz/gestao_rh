7f46142c6bc87c0c10ffdf61664bb97bb17f2e56
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-05-15 20:39:10 -0300

    finalizando crud Funcionarios + crud updateviews

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import FuncionariosList, FuncionarioEdit
 
 urlpatterns = [
-    path('', home),
+    path('', FuncionariosList.as_view(), name='list_funcionarios'),
+    path('editar/<int:pk>/', FuncionarioEdit.as_view(), name='update_funcionario'),
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,7 +1,14 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.views.generic import ListView, UpdateView
+from .models import Funcionario
 
 # Create your views here.
-def home(request):
-    return HttpResponse('Ola')
+class FuncionariosList(ListView):
+    model = Funcionario
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ['name', 'departamentos']
